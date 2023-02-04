f22226902982eab412ede21d9a6f72553931a7ac
ELISENWOLF <aravindprakash2001@gmail.com>
2023-02-04 21:39:29 +0530

    modified

--- Portfolio-ThreeJS/main.js
+++ Portfolio-ThreeJS/main.js
@@ -4,26 +4,26 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer =  new THREE.WebGL1Renderer({
+const renderer = new THREE.WebGL1Renderer({
     canvas: document.querySelector('#bg'),
 });
 
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render( scene, camera );
+renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
 const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
-const torus = new THREE.Mesh( geometry, material );
+const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus)
 
 const pointLight = new THREE.PointLight(0xfffff);
-pointLight.position.set(5,5,5);
+pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
@@ -38,9 +38,9 @@ const addStar = () => {
 
     const geometry = new THREE.SphereGeometry(0.25, 24, 24);
     const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-    const star = new THREE.Mesh( geometry, material );
+    const star = new THREE.Mesh(geometry, material);
 
-    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 500 ));
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(500));
 
     star.position.set(x, y, z);
     scene.add(star)
@@ -77,15 +77,15 @@ moon.position.setY(5).setX(-15).setZ(10)
 scene.add(moon)
 
 const animate = () => {
-    requestAnimationFrame( animate );
- 
+    requestAnimationFrame(animate);
+
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.01;
 
     controls.update();
-     
-    renderer.render( scene, camera );
+
+    renderer.render(scene, camera);
 }
 
 animate();
